c7460b6240eb4cfc8f6df11b2291ead863010055
bncastle <bncastle@gmail.com>
2016-08-12 14:02:58 -0400

    First Code Entry
    Added Code
    Updated README

---
+++ HR.hx
@@ -0,0 +1,182 @@
+import sys.FileSystem;
+import sys.io.File;
+import haxe.Json;
+
+// @author Bryan Castleberry
+//
+//The comments below are meant to utilize my Hix utility to compile/run
+//without needing a build.hxml or command line options. Hix can be found here:
+//https://github.com/bncastle/hix
+//
+//::hix -main HR --no-traces -dce full -cpp bin
+//::hix:neko -main HR --no-traces -dce full -neko hr.n
+
+//This lets Haxe know what structure we expect in our JSON task file
+//
+typedef Task = 
+{
+	var name: String;
+	var cmds: Array<String>;
+}
+
+//typedef Cfg =
+//{
+	//var shell:String;
+	//var tasks: Array<Task> ;
+//}
+
+class HR
+{
+	static inline var VERSION = "0.1";
+	static inline var CFG_FILE = "tasks.json";
+	static inline var ERR_TASK_NO_FOUND = -1025;
+	var tasks:Array<Task>;
+	
+	static function main(): Int
+	{
+		Sys.println('HR Version $VERSION): A simple Task runner.');
+		Sys.println("Copyright 2016 Pixelbyte Studios");
+		
+		//Note this: We want the config file in the directory where we were invoked!
+		var cfgFile:String = Sys.getCwd() + CFG_FILE;
+		
+		if (Sys.args().length < 1)
+		{
+			Sys.println("Usage: HR.exe <task_name>");
+			Sys.println("HR.exe -t (prints a list of valid tasks)");
+			
+			if (!HR.CheckForConfigFile(cfgFile))
+			{
+				Sys.println("Make a tasks.json file and put it in your project directory.");
+				Sys.println("EX: [ {\"name\":\"task1\",\"cmds\": [\"cmd1\", \"cmd2\"] } ]");
+				Sys.println("You can a task by specifying its name");
+				Sys.println("If you want to run a task within a task, precede the task label by ':'");
+				Sys.println("If you don't want task's command to be displayed when its run, add '!' to the beginning of the command");
+				Sys.println("HR.exe task1");
+			}
+			return -1;
+		}
+		
+		var h = new HR();
+		if (!h.ParseConfig(cfgFile))
+		{
+			return -1;
+		}
+		
+		if (Sys.args()[0] == "-t")
+		{
+			h.PrintAvailableTasks();
+			return -1;
+		}
+
+		var taskName:String = Sys.args()[0];
+		var retCode:Int = h.RunTask(taskName);
+		if (retCode == ERR_TASK_NO_FOUND)
+		{
+			Sys.println("Task: " + taskName + " not found!");
+			return -1;
+		}
+		else if ( retCode != 0)
+		{
+			Sys.println("Error running task: " + taskName);
+			return -1;
+		}
+		
+		return retCode;
+	}
+	
+	public function new() { }
+	
+	function ParseConfig(cfgFile:String): Bool
+	{	
+		if (!HR.CheckForConfigFile(cfgFile)) return false;
+		
+		//Grab all the text from the file
+		var text = File.getContent(cfgFile);
+		
+		//Parse the JSON
+		tasks = Json.parse(text);
+
+		if (tasks.length < 1)
+		{
+			Sys.println("No tasks found in " + CFG_FILE);
+			return false;
+		}
+		
+		//for (i in 0 ... tasks.length ) 
+		//{
+			//if (tasks[i].name == "")
+				//tasks.splice(i, 1);
+		//}
+
+		return true;
+	}
+
+	function RunTask(name: String):Int
+	{
+		for	(t in tasks)
+		{
+			if (name.toLowerCase() == t.name.toLowerCase())
+			{
+				if (t.cmds.length == 0)
+				{
+					Sys.println("No commands found for task: " + name);
+					return -1;
+				}
+				else
+				{
+					for (cmd in t.cmds)
+					{
+						//See if the command is itself a task. If it is, run it
+						if(cmd.charAt(0) == ":")
+						{
+							var retcode:Int = RunTask(cmd.substr(1));
+							if (retcode != 0) return retcode;
+						}
+						else
+						{
+							//If there is a ! at the beginning, that means Dont print the command when running it
+							if(cmd.charAt(0)!="!")
+								Sys.println(cmd);
+							else
+								cmd = cmd.substr(1); //remove the "!" from the command								
+							var retcode:Int = Sys.command(cmd);
+							if (retcode != 0) return retcode;
+						}
+					}
+					return 0;
+				}
+			}
+		}
+		return ERR_TASK_NO_FOUND;
+	}
+
+	function PrintAvailableTasks()
+	{
+		Sys.println("Available Tasks:");
+		for	(t in tasks)
+		{
+			Sys.println(t.name);
+		}
+	}
+	
+	//function IsTask(name:String)
+	//{
+	//	for (t in tasks) 
+	//	{
+	//		if (name.toLowerCase() == t.name.toLowerCase()) return true;
+	//	}
+	//	return false;
+	//}
+	
+	static function CheckForConfigFile(cfgFile:String): Bool
+	{
+		//Check for a valid config file
+		if (!FileSystem.exists(cfgFile))
+		{
+			Sys.print("Config file " + cfgFile + " doesn't exist.");
+			return false;
+		}
+		return true;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # HR
 A very simple task runner written in Haxe
+
+## Building
+
+(Note: You will want to grab [Hix](https://github.com/bncastle/hix) which is my Haxe build tool for building.
+Or, you can look in the HR.hx source file and tease out the compiler flags to feed the Haxe compiler.)
+
+Open a command line in the project and type:
+
+```hix HR.hx```
+
+This will build HR as a cpp project, and the .exe will be found in ```bin``` directory.
+
+## Usage
+
+HR uses a ```tasks.json``` file located in the root of the directory where you want to run the tasks. Each
+task is a JSON object containing a "name" property and a "cmds" array where command strings are specified.
+Each task can be composed of just one command, multiple commands, or other tasks can also be specified as 
+a command.An example ```tasks.json``` file is shown below:
+
+```json
+
+[
+    {
+        "name":"directory",
+        "cmds":["dir"]
+    },
+    {
+        "name":"dual",
+        "cmds":[":directory","copy /?"]
+    },
+    {
+        "name":"speak",
+        "cmds":["!echo hello"]
+    }
+]
+
+```
+
+To run the "directory" task, you type:
+
+```HR directory```
+
+This will run all the commands contained within the directory task until a command returns non-zero (i.e. an error).
+When a task is executed, each command will be printed to the console unless it is  prefixed by a "!". This
+As previously mentioned, other tasks can also be executed as commands by prefacing them with a ":". The
+"dual" command is an example of this as its first command is to run the "directory" task, then print the help on the
+DOS copy command.
+
+Available tasks can be listed by typing:
+
+```HR -t ```
+
+Well that's it for now. It's a pretty simple program mostly written as an exercise for me. More will come later.
